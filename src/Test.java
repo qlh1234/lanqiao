f96bddad97d269dd128a7a3c0420911874d39909
lenovo <2840328103@qq.com>
2020-12-29 20:44:57 +0800

    用户

---
+++ src/Test.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("请输入用户名：");
+        Scanner sc = new Scanner(System.in);
+        String username = sc.next();
+        System.out.println("你输入的用户名是："+ username);
+
+        System.out.println("请输入密码：");
+
+        String password = sc.next();
+        System.out.println("你输入的用户名是："+ password);
+
+        File file = new File("C:\\Users\\lenovo\\IdeaProjects\\CmdShop\\src\\Users.xlsx");
+        ReadExcel readExcel = new ReadExcel();
+        User users[] =readExcel.readExcel(file);
+        for (int i =1;i<users.length;i++){
+            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())){
+
+                System.out.println("登录成功");
+           break;
+
+            }else{
+               System.out.println("登录失败");
+            }
+        }
+
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,38 @@
+public class User {
+    String Username;
+    String Phone;
+    String Address;
+    String Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
